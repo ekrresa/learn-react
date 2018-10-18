08c3ebfad953fb274ee6175ee0ea01e650e3e071
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2018-10-19 00:23:54 +0100

    Updates

---
+++ precache-manifest.06f06a87df34104af624009b9b650cce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ac09455f0ec8c2766dd",
+    "url": "/learn-react/static/js/runtime~main.2ac09455.js"
+  },
+  {
+    "revision": "f8e2dfe87e8f4e19114c",
+    "url": "/learn-react/static/js/main.f8e2dfe8.chunk.js"
+  },
+  {
+    "revision": "0ff9a1462043b699e9f4",
+    "url": "/learn-react/static/js/1.0ff9a146.chunk.js"
+  },
+  {
+    "revision": "f8e2dfe87e8f4e19114c",
+    "url": "/learn-react/static/css/main.ca60db0a.chunk.css"
+  },
+  {
+    "revision": "0ff9a1462043b699e9f4",
+    "url": "/learn-react/static/css/1.5e3bce55.chunk.css"
+  },
+  {
+    "revision": "0654abfe623602304e64cf315abc1da6",
+    "url": "/learn-react/index.html"
+  }
+];
